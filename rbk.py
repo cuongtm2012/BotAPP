3d9ddb70c8f57a53812cfd2e048a34262041eab5
Trần Cường <cuongtm2012@gmail.com>
2023-08-17 07:10:42 +0700

    1708 update rbk

--- rbk.py
+++ rbk.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from collections import defaultdict
 import re
+from collections import Counter
 
 try:
-    with open("unique_numbers.txt", "a") as file:
+    with open("unique_numbers.txt", "a", encoding="utf-8") as file:
         # Get today's date in the format YYYY-MM-DD
         today_date = datetime.today().strftime('%Y-%m-%d')
         # Create the URL with the today's date
@@ -32,10 +33,7 @@ try:
             total_unique_numbers = len(unique_numbers_set)
             file.write("Today Date : " + today_date + "\n")
             file.write("Unique Numbers: " + formatted_output + "\n")
-            file.write("Total Unique Numbers: " + str(total_unique_numbers) + "\n")
-
-            print("Output written to 'unique_numbers.txt'")
-            
+            file.write("Total Unique Numbers: " + str(total_unique_numbers) + "\n")            
         else:
             print(
                 f"Failed to fetch data from {url}. Status code: {response.status_code}")
@@ -97,47 +95,56 @@ try:
                 for idx, post in enumerate(post_divs, start=1):
                     message_content = post.find("div", class_="messageContent")
                     message_meta = post.find("div", class_="messageMeta")
+                    data_author_value = post['data-author']
 
-                    if message_content and message_meta:
+                    if message_content and message_meta and data_author_value != "quedau1981":
                         content_text = message_content.get_text().strip()
                         meta_text = message_meta.get_text().strip()
+                        
+                        # Split the text into parts using comma and strip extra spaces
+                        parts = [part.strip() for part in meta_text.split(',')]
+                        
+                        # Extract the date and time
+                        date_time = parts[1].split(' ')
+                        date = date_time[0]
+                        time = date_time[2].split()[0]
+
+                        # Construct the datetime object for the extracted date and time
+                        full_date_time = datetime.strptime(
+                            f"{date} {time}", "%d/%m/%y %H:%M")
+
+                        # Calculate yesterday's date and time (18:30)
+                        yesterday = datetime.now() - timedelta(days=1)
+                        target_time = yesterday.replace(hour=18, minute=30)
+                        
+                        # Calculate today's date and time (18:30)
+                        today_date = datetime.now() - timedelta(days=0)
+                        full_today_date = today_date.replace(hour=18, minute=30)
+                        
+                            # Calculate yesterday's date and time (18:30)
+                        yesterday_two = datetime.now() - timedelta(days=2)
+                        target_time_two = yesterday_two.replace(hour=18, minute=30)
+
+                        # Compare and break if the condition is met
+                        if full_date_time < target_time or full_date_time > full_today_date:
+                            continue
+                        
+                        if full_date_time < target_time_two:
+                            continue_loop = False
+                            break
 
                         # Filter out lines containing "Ngày"
                         content_lines = [line for line in content_text.split(
                             '\n') if "Ngày" not in line]
 
-                        if "9x" in content_text and "Mức" not in content_text:
-                            with open("unique_numbers.txt", "a") as file:
+                        if "9x" in content_text and "Mức" not in content_text and "TH" not in content_text:
+                            with open("unique_numbers.txt", "a", encoding="utf-8") as file:
                                 file.write(f"Post {idx}:\n")
                                 file.write("Content:\n")
-                                file.write('\n'.join(content_lines))
+                                file.write('\n'.join(content_lines) + "\n")
 
-                            # Split the text into parts using comma and strip extra spaces
-                            parts = [part.strip() for part in meta_text.split(',')]
-
-                            # Extract the author
-                            author = parts[0].replace("Meta: ", "")
-
-                            # Extract the date and time
-                            date_time = parts[1].split(' ')
-                            date = date_time[0]
-                            time = date_time[2].split()[0]
-
-                            # Construct the datetime object for the extracted date and time
-                            full_date_time = datetime.strptime(
-                                f"{date} {time}", "%d/%m/%y %H:%M")
-
-                            # Calculate yesterday's date and time (18:30)
-                            yesterday = datetime.now() - timedelta(days=1)
-                            target_time = yesterday.replace(hour=18, minute=30)
-
-                            # Compare and break if the condition is met
-                            if full_date_time < target_time:
-                                continue_loop = False
-                                break
-
-                            with open("unique_numbers.txt", "a") as file:   
-                                file.write(f"Author: {author}\n")
+                            with open("unique_numbers.txt", "a", encoding="utf-8") as file:   
+                                file.write(f"Author: {data_author_value}\n")
                                 file.write(f"Date: {date}\n")
                                 file.write(f"Time: {time}\n")
                                 file.write("=" * 50 + "\n")
@@ -150,19 +157,23 @@ try:
                             if matches:
                                 numbers_string = matches.group(1)
                                 numbers_array = numbers_string.split(',')
-                                numbers_array = [
-                                    num.strip() for num in numbers_array if num.strip().isdigit()]
+                                numbers_array = [num.strip() for num in numbers_array if num.strip().isdigit()]
 
-                                with open("unique_numbers.txt", "a") as file:
-                                    file.write(f"1x array: {numbers_array} \n")
-                                # Accumulate numbers from all articles
-                                all_numbers_array.extend(numbers_array)
+                                with open("unique_numbers.txt", "a", encoding="utf-8") as file:
+                                    file.write(f"0x array: {numbers_array} \n")
 
-                                # Store the appearing time of each number
-                                for num in numbers_array:
+                                # Count the occurrences of each number in the current post
+                                num_counts = Counter(numbers_array)
+
+                                # Update the appearing time of each number in the number_appearing_time dictionary
+                                for num, count in num_counts.items():
                                     if num not in number_appearing_time:
                                         # Store the post index as appearing time
                                         number_appearing_time[num] = idx
+
+                                # Accumulate numbers from all articles
+                                all_numbers_array.extend(numbers_array)
+
                             else:
                                 print("Pattern not found in input text.")
                 # Move to the next page
@@ -189,7 +200,7 @@ try:
             numbers.sort(key=lambda num: number_appearing_time.get(num, 0))  # Sort by appearing time
             numbers_text = ', '.join(numbers)
             
-            with open("unique_numbers.txt", "a") as file:
+            with open("unique_numbers.txt", "a", encoding="utf-8") as file:
                 file.write(f"Numbers appearing {count} times: {numbers_text} ,\n")
     file.close()
 
