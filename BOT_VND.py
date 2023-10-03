6a13a6baebe6164dbca46497451fb6275efc9f68
cuong <cuongtm2012@gmail.com>
2023-10-03 16:13:00 +0700

    update BOT CKVN

--- BOT_VND.py
+++ BOT_VND.py
@@ -1,62 +1,85 @@
+import schedule
+import time as t  # Rename the time module to 't' to avoid conflicts
+from slack_sdk import WebClient
 import requests
+import configparser
 from bs4 import BeautifulSoup
-import yfinance as yf
+from datetime import datetime, timedelta
+from collections import defaultdict
+import re
+from collections import Counter
 
-# Đường dẫn đến trang web cần lấy dữ liệu
-url = "https://www.cophieu68.vn/stats/volume_buzz.php"
+# Load configuration from config.ini
+config = configparser.ConfigParser()
+config.read("config.ini")
+# config.read("/root/BotAPP/config.ini")
 
-# Gửi yêu cầu GET đến trang web
-response = requests.get(url)
+# Initialize the Slack WebClient with your Slack app's token
+slack_token = config["Slack"]["slack_token"]
+slack_channel = "#ckvn"
+slack_client = WebClient(token=slack_token)
 
-# Kiểm tra xem yêu cầu có thành công hay không
-if response.status_code == 200:
-    # Sử dụng BeautifulSoup để phân tích HTML
-    soup = BeautifulSoup(response.text, "html.parser")
+# Define the variable to store message content
+message_content = ""
 
-    # Tìm thẻ HTML chứa nội dung bạn muốn lấy (class "table_content")
-    table_content = soup.find("table", class_="table_content")
+def send_to_slack(message):
+    print("Success send to slack " + message + "\n")
+    slack_client.chat_postMessage(channel=slack_channel, text=message)
 
-    if table_content:
-        # Tìm tất cả các dòng trong bảng (thẻ <tr>)
-        rows = table_content.find_all("tr")
 
-        # Tạo danh sách để lưu các đối tượng
-        data_list = []
+def run_and_send_to_slack():
+    output_to_send = ""
 
-        # Bỏ qua dòng đầu tiên vì nó chứa tiêu đề
-        for row in rows[1:]:
-            # Tìm tất cả các ô dữ liệu trong dòng (thẻ <td>)
-            cells = row.find_all("td")
+    try:
+        url = "https://www.cophieu68.vn/stats/volume_buzz.php"
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            table_content = soup.find("table", class_="table_content")
 
-            if len(cells) == 12:  # Đảm bảo rằng có đúng 8 ô dữ liệu
-                # Trích xuất dữ liệu từ mỗi ô
-                symbol = cells[1].text.strip()
-                price = cells[2].text
-                change = cells[3].text
-                change_percent = cells[4].text
-                volume = cells[5].text
-                volume_10D = cells[6].text
-                volume_10ratio = float(cells[7].text)
+            if table_content:
+                rows = table_content.find_all("tr")
+                data_list = []
+                for row in rows[1:]:
+                    cells = row.find_all("td")
 
-                # Tạo đối tượng từ dữ liệu và thêm vào danh sách
-                data_obj = {
-                    "Symbol": symbol,
-                    "Price": price,
-                    "Change": change,
-                    "Change_Percent": change_percent,
-                    "volume": volume,
-                    "volume_10D": volume_10D,
-                    "volume_10ratio": volume_10ratio
-                }
-                data_list.append(data_obj)
+                    if len(cells) == 12:  # Đảm bảo rằng có đúng 8 ô dữ liệu
+                        # Trích xuất dữ liệu từ mỗi ô
+                        symbol = cells[1].text.strip()
+                        price = cells[2].text
+                        change = cells[3].text
+                        change_percent = cells[4].text
+                        volume = cells[5].text
+                        volume_10D = cells[6].text
+                        volume_10ratio = float(cells[7].text)
 
-        filtered_data = [item for item in data_list if item['volume_10ratio'] > 1.0 and int(
-            item['volume_10D'].replace(',', '')) > 1000000]
+                        # Tạo đối tượng từ dữ liệu và thêm vào danh sách
+                        data_obj = {
+                            "Symbol": symbol,
+                            "Price": price,
+                            "Change": change,
+                            "Change_Percent": change_percent,
+                            "volume": volume,
+                            "volume_10D": volume_10D,
+                            "volume_10ratio": volume_10ratio
+                        }
+                        data_list.append(data_obj)
 
-        for item in filtered_data:
-            print(item)
+                filtered_data = [item for item in data_list if item['volume_10ratio'] > 1.1 and int(
+                    item['volume_10D'].replace(',', '')) > 1000000]
 
-    else:
-        print("Can not found class 'table_content'")
-else:
-    print("Can not get data from Web.")
+                for item in filtered_data:
+                    item_string = f"Symbol: {item['Symbol']}, Price: {item['Price']}, Change: {item['Change']}, Change_Percent: {item['Change_Percent']}, volume: {item['volume']}, volume_10D: {item['volume_10D']}, vol_ratio: {item['volume_10ratio']}\n"
+                    output_to_send += item_string
+
+            else:
+                output_to_send += "Can not found class 'table_content' \n"
+        else:
+            output_to_send += "Can not get data from Web. \n"
+        send_to_slack(output_to_send)
+    except Exception as e:
+        error_message = f"An error occurred: {e}"
+        send_to_slack(error_message)
+
+
+run_and_send_to_slack()
