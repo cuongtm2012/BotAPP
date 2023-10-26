22fe8f2462aeb75d758bf8718e4023b76dcb061d
Trần Cường <cuongtm2012@gmail.com>
2023-10-26 23:47:45 +0700

    update BOT OLBG

--- KQ_OLBG.py
+++ KQ_OLBG.py
@@ -1,5 +1,7 @@
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
+import pytz
 
 # URL của trang web bạn muốn lấy dữ liệu
 url = "https://www.olbg.com/betting-tips/Football/1"
@@ -13,28 +15,44 @@ soup = BeautifulSoup(response.text, "html.parser")
 # Tìm tất cả các thẻ div có lớp "tips-ls open"
 tips_divs = soup.find_all("div", class_="tips-ls")
 
-skip_div = False
+source_timezone = pytz.timezone('Europe/London')
+vietnam_timezone = pytz.timezone('Asia/Ho_Chi_Minh')
 # Lặp qua danh sách các thẻ div và lấy nội dung của mỗi thẻ
 for div in tips_divs:
     li_items = div.find_all("li")
     for li in li_items:
         event_info = li.find("div", class_="rw evt")
-        match_info = event_info.find("a", itemprop="url").text.strip()
-        league = event_info.find("span", class_="h-ellipsis").text.strip()
-        date_time = event_info.find("time", itemprop="startDate")["content"]
-
-        # Trích xuất thông tin về selection
-        selection_info = li.find("div", class_="rw slct")
-        selection = selection_info.find("a", class_="h-rst-lnk").text.strip()
-
-        # Trích xuất độ chính xác của tip
-        tips_info = li.find("div", class_="rw tips")
-        tip_accuracy = tips_info.find("b", class_="h-ellipsis").text.strip()
-
-        # In thông tin trận đấu, giải đấu, ngày giờ, selection và độ chính xác của tip
-        print("Match:", match_info)
-        print("League:", league)
-        print("Date and Time:", date_time)
-        print("Selection:", selection)
-        print("Tip Accuracy:", tip_accuracy)
-        print()
+        
+        # Kiểm tra xem event_info có tồn tại trước khi sử dụng find trên nó
+        if event_info:
+            match_info_elem = event_info.find("a", itemprop="url")
+            
+            # Kiểm tra xem match_info_elem có tồn tại hay không
+            if match_info_elem:
+                date_time_elem = event_info.find("time", itemprop="startDate")
+                
+                # Kiểm tra xem date_time_elem có tồn tại và có thuộc tính "datetime" hay không
+                if date_time_elem and "datetime" in date_time_elem.attrs:
+                    date_time_str = date_time_elem["datetime"]
+                    date_time = datetime.fromisoformat(date_time_str)
+                    
+                    # Kiểm tra năm của trận đấu
+                    if date_time.year == 2023:
+                        league = event_info.find("span", class_="h-ellipsis").text.strip()
+                        date_time_gmt7 = date_time.astimezone(vietnam_timezone)
+                        formatted_date_time = date_time_gmt7.strftime("%Y-%m-%d %H:%M")
+                        
+                        selection_info = li.find("div", class_="rw slct")
+                        selection = selection_info.find("a", class_="h-rst-lnk").text.strip()
+                        
+                        tips_info = li.find("div", class_="rw tips")
+                        tip_accuracy = tips_info.find("b", class_="h-ellipsis").text.strip()
+                        
+                        print("Match:", match_info_elem.text.strip())
+                        print("League:", league)
+                        print("Date and Time (GMT+7):", formatted_date_time)
+                        print("Selection:", selection)
+                        print("Tip Accuracy:", tip_accuracy)
+                        print()
+        else:
+            print("N/A")
